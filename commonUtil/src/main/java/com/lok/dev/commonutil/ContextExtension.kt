d73932f6af9024f84d71a28d2ded1ece6b731ca6
HyemiOh <hb5536@gmail.com>
2023-02-09 00:23:13 +0900

    [수정] 과목 선택 다이얼로그 spinner 커스텀

---
+++ commonUtil/src/main/java/com/lok/dev/commonutil/ContextExtension.kt
@@ -0,0 +1,6 @@
+package com.lok.dev.commonutil
+
+import android.content.Context
+import android.util.TypedValue
+
+fun Context.convertDpToPx(dp: Float) = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, this.resources.displayMetrics)
