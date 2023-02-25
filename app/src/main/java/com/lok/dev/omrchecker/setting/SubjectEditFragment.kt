19a339882e8edcc01f5b4e2b08c85b25a9f4e510
HyemiOh <hb5536@gmail.com>
2023-02-25 11:31:38 +0900

    [setting] 과목편집 ui 작업중

---
+++ app/src/main/java/com/lok/dev/omrchecker/setting/SubjectEditFragment.kt
@@ -0,0 +1,40 @@
+package com.lok.dev.omrchecker.setting
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.lok.dev.commonbase.BaseFragment
+import com.lok.dev.commonmodel.state.SubjectState
+import com.lok.dev.commonutil.throttleFirstClick
+import com.lok.dev.omrchecker.databinding.FragmentSubjectAddBinding
+import com.lok.dev.omrchecker.setting.viewmodel.SettingViewModel
+import javax.inject.Inject
+
+class SubjectEditFragment @Inject constructor() : BaseFragment<FragmentSubjectAddBinding>() {
+
+    private val viewModel by activityViewModels<SettingViewModel>()
+
+    override val enableBackPressed = true
+
+    override fun createFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ) = FragmentSubjectAddBinding.inflate(inflater, container, false)
+
+    override fun initFragment() {
+        addListeners()
+    }
+
+    private fun addListeners() = with(binding) {
+        throttleFirstClick(tvSave) {
+            if(etSubjectTitle.text.isNotEmpty()) {
+                viewModel.addSubject(etSubjectTitle.text.toString())
+                viewModel.setSubjectState(SubjectState.Select)
+            }
+        }
+    }
+
+    override fun onFragmentBackPressed() {
+        viewModel.setSubjectState(SubjectState.Select)
+    }
+}
